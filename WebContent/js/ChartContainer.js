55b7a3da8cac47f89224a48b15d87680ebde94ec
Juan Pablo Rivillas Ospina <jprivillaso@gmail.com>
2013-07-31 11:40:40 -0300

    Modified views and controllers to display two type of charts

--- WebContent/js/ChartContainer.js
+++ WebContent/js/ChartContainer.js
@@ -1,115 +1,137 @@
 (function(){
 	
-	var displayChart = function(){
+	var displaySplineChart = function(){
 		$('#ChartContainer-Chart').highcharts({
             chart: {
                 type: 'spline'
             },
             title: {
-                text: 'Snow depth in the Vikjafjellet mountain, Norway'
+                text: 'Monthly Average Temperature'
             },
             subtitle: {
-                text: 'An example of irregular time data in Highcharts JS'
+                text: ''
             },
             xAxis: {
-                type: 'datetime',
-                dateTimeLabelFormats: { // don't display the dummy year
-                    month: '%e. %b',
-                    year: '%b'
-                }
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
             },
             yAxis: {
                 title: {
-                    text: 'Snow depth (m)'
+                    text: 'Temperature'
                 },
-                min: 0
+                labels: {
+                    formatter: function() {
+                        return this.value +'°';
+                    }
+                }
+            },
+            tooltip: {
+                crosshairs: true,
+                shared: true
+            },
+            plotOptions: {
+                spline: {
+                    marker: {
+                        radius: 4,
+                        lineColor: '#666666',
+                        lineWidth: 1
+                    }
+                }
+            },
+            series: [{
+                name: 'Tokyo',
+                marker: {
+                    symbol: 'square'
+                },
+                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
+                    y: 26.5,
+                    marker: {
+                        symbol: 'url(http://www.highcharts.com/demo/gfx/sun.png)'
+                    }
+                }, 23.3, 18.3, 13.9, 9.6]
+    
+            }, {
+                name: 'London',
+                marker: {
+                    symbol: 'diamond'
+                },
+                data: [{
+                    y: 3.9,
+                    marker: {
+                        symbol: 'url(http://www.highcharts.com/demo/gfx/snow.png)'
+                    }
+                }, 9.2, 5.7, 8.5, 11.9, 15.2, 2.0, 16.6, 14.2, 10.3, 5, 4.8]
+            }]
+        });
+	};
+	
+	var displayRealTimeChart = function(){
+        $('#ChartContainer-Chart').highcharts({
+            chart: {
+                type: 'spline',
+                animation: Highcharts.svg, // don't animate in old IE
+                marginRight: 10,
+                events: {
+                    load: function() {
+    
+                        // set up the updating of the chart each second
+                        var series = this.series[0];
+                        setInterval(function() {
+                            var x = (new Date()).getTime(), // current time
+                                y = Math.random();
+                            series.addPoint([x, y], true, true);
+                        }, 1000);
+                    }
+                }
+            },
+            title: {
+                text: 'Live random data'
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 150
+            },
+            yAxis: {
+                title: {
+                    text: 'Value'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
             },
             tooltip: {
                 formatter: function() {
                         return '<b>'+ this.series.name +'</b><br/>'+
-                        Highcharts.dateFormat('%e. %b', this.x) +': '+ this.y +' m';
+                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
+                        Highcharts.numberFormat(this.y, 2);
                 }
             },
-            
+            legend: {
+                enabled: false
+            },
+            exporting: {
+                enabled: false
+            },
             series: [{
-                name: 'Winter 2007-2008',
-                // Define the data points. All series have a dummy year
-                // of 1970/71 in order to be compared on the same x axis. Note
-                // that in JavaScript, months start at 0 for January, 1 for February etc.
-                data: [
-                    [Date.UTC(1970,  9, 27), 0   ],
-                    [Date.UTC(1970, 10, 10), 0.6 ],
-                    [Date.UTC(1970, 10, 18), 0.7 ],
-                    [Date.UTC(1970, 11,  2), 0.8 ],
-                    [Date.UTC(1970, 11,  9), 0.6 ],
-                    [Date.UTC(1970, 11, 16), 0.6 ],
-                    [Date.UTC(1970, 11, 28), 0.67],
-                    [Date.UTC(1971,  0,  1), 0.81],
-                    [Date.UTC(1971,  0,  8), 0.78],
-                    [Date.UTC(1971,  0, 12), 0.98],
-                    [Date.UTC(1971,  0, 27), 1.84],
-                    [Date.UTC(1971,  1, 10), 1.80],
-                    [Date.UTC(1971,  1, 18), 1.80],
-                    [Date.UTC(1971,  1, 24), 1.92],
-                    [Date.UTC(1971,  2,  4), 2.49],
-                    [Date.UTC(1971,  2, 11), 2.79],
-                    [Date.UTC(1971,  2, 15), 2.73],
-                    [Date.UTC(1971,  2, 25), 2.61],
-                    [Date.UTC(1971,  3,  2), 2.76],
-                    [Date.UTC(1971,  3,  6), 2.82],
-                    [Date.UTC(1971,  3, 13), 2.8 ],
-                    [Date.UTC(1971,  4,  3), 2.1 ],
-                    [Date.UTC(1971,  4, 26), 1.1 ],
-                    [Date.UTC(1971,  5,  9), 0.25],
-                    [Date.UTC(1971,  5, 12), 0   ]
-                ]
-            }, {
-                name: 'Winter 2008-2009',
-                data: [
-                    [Date.UTC(1970,  9, 18), 0   ],
-                    [Date.UTC(1970,  9, 26), 0.2 ],
-                    [Date.UTC(1970, 11,  1), 0.47],
-                    [Date.UTC(1970, 11, 11), 0.55],
-                    [Date.UTC(1970, 11, 25), 1.38],
-                    [Date.UTC(1971,  0,  8), 1.38],
-                    [Date.UTC(1971,  0, 15), 1.38],
-                    [Date.UTC(1971,  1,  1), 1.38],
-                    [Date.UTC(1971,  1,  8), 1.48],
-                    [Date.UTC(1971,  1, 21), 1.5 ],
-                    [Date.UTC(1971,  2, 12), 1.89],
-                    [Date.UTC(1971,  2, 25), 2.0 ],
-                    [Date.UTC(1971,  3,  4), 1.94],
-                    [Date.UTC(1971,  3,  9), 1.91],
-                    [Date.UTC(1971,  3, 13), 1.75],
-                    [Date.UTC(1971,  3, 19), 1.6 ],
-                    [Date.UTC(1971,  4, 25), 0.6 ],
-                    [Date.UTC(1971,  4, 31), 0.35],
-                    [Date.UTC(1971,  5,  7), 0   ]
-                ]
-            }, {
-                name: 'Winter 2009-2010',
-                data: [
-                    [Date.UTC(1970,  9,  9), 0   ],
-                    [Date.UTC(1970,  9, 14), 0.15],
-                    [Date.UTC(1970, 10, 28), 0.35],
-                    [Date.UTC(1970, 11, 12), 0.46],
-                    [Date.UTC(1971,  0,  1), 0.59],
-                    [Date.UTC(1971,  0, 24), 0.58],
-                    [Date.UTC(1971,  1,  1), 0.62],
-                    [Date.UTC(1971,  1,  7), 0.65],
-                    [Date.UTC(1971,  1, 23), 0.77],
-                    [Date.UTC(1971,  2,  8), 0.77],
-                    [Date.UTC(1971,  2, 14), 0.79],
-                    [Date.UTC(1971,  2, 24), 0.86],
-                    [Date.UTC(1971,  3,  4), 0.8 ],
-                    [Date.UTC(1971,  3, 18), 0.94],
-                    [Date.UTC(1971,  3, 24), 0.9 ],
-                    [Date.UTC(1971,  4, 16), 0.39],
-                    [Date.UTC(1971,  4, 21), 0   ]
-                ]
+                name: 'Random data',
+                data: (function() {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(), i;
+    
+                    for (i = -19; i <= 0; i++) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: Math.random()
+                        });
+                    }
+                    return data;
+                })()
             }]
         });
-	};
+	};	
 	
 	brite.registerView("ChartContainer", {emptyParent : true}, {
 		create: function(){
@@ -117,13 +139,30 @@
 		},
 		
 		postDisplay: function(){
-			console.log('disp chart');
 		}, 
 		
 		events: {
-			'click; .showTableBtn':function(){
-				console.log('about to display the table');
-				displayChart();
+			'click; .showTableBtn':function(event){
+				this.$el.trigger('DO_SELECT_SHOW_CHART');
+			},
+		
+			'click; .hideTableBtn':function(event){
+				console.log('about to hide the table');			
+			}
+		},
+		
+		docEvents:{
+			'DO_SELECT_SHOW_CHART': function(event, type){
+				switch(type){
+					case 'spline' :
+						displaySplineChart();
+						break;
+					case 'realTime' :
+						displayRealTimeChart();
+						break;
+					default:
+						break;
+				}
 			}
 		}
 	});	
--- WebContent/js/MenuContainer.js
+++ WebContent/js/MenuContainer.js
@@ -10,6 +10,14 @@
 		events: {
 			'click; .displayChartBtn':function(){
 				console.log('button pressed');
+			},
+			
+			'click; #type-line': function(){
+				this.$el.trigger("DO_SELECT_SHOW_CHART", 'spline');
+			},
+			
+			'click; #type-realTime': function(){
+				this.$el.trigger("DO_SELECT_SHOW_CHART", 'realTime');
 			}
 		}
 	});
